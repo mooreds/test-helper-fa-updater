05b0010547a1d030b29d6255d54a719aa14694c1
Dan Moore <dan@fusionauth.io>
2024-08-15 14:12:36 -0600

    pass latest released version around

--- bin/pushtheme.sh
+++ bin/pushtheme.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+
+# get theme from command line
+if [ "x$1" != "x" ]; then
+  latest_released_version=$1
+fi
+
+if [ "x$latest_released_version" == "x" ]; then
+  echo "no released version var found"
+  exit 1
+fi
+
+echo $latest_released_version
+
 # copy to history
+cd fusionauth-theme-helper
 
 cp tmp/*.ftl ../fusionauth-theme-history/themes
 
@@ -9,9 +24,9 @@ git status
 
 git commit -m "$latest_released_version themes" .
 git tag $latest_released_version
+git tag
 
 # push that up
-git tag
 
 # TODO
 
