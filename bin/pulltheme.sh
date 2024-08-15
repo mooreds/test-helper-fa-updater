34e3ce4a251abbff4022a00dd604ef778048c570
Dan Moore <dan@fusionauth.io>
2024-08-15 13:20:39 -0600

    make pull theme work

--- bin/pulltheme.sh
+++ bin/pulltheme.sh
@@ -1,35 +1,8 @@
-
-# pull latest version from account
-latest_released_version=`curl --silent https://account.fusionauth.io/api/version -o - | jq 'last(.versions[-1])'|sed 's/"//g'`
-
-# let us override to pull down a particular version
-if [ "x$1" != "x" ]; then
-  latest_released_version=$1
-fi
-
-# pull the theme-history down
-
-git clone git@github.com:FusionAuth/fusionauth-theme-history.git fusionauth-theme-history
-cd fusionauth-theme-history
-git pull origin --tags
-
-latest_theme_version=`git tag|tail -1`
-
-# see if they match
-# if they do, exit
-if [ $latest_released_version == $latest_theme_version ]; then
-  echo "versions the same"
-  exit 0
-fi
-
-# if they do not then
+#!/bin/bash
 
 # pull down theme helper
 cd ..
 git clone git@github.com:FusionAuth/fusionauth-theme-helper.git fusionauth-theme-helper
-
-# stand up fusionauth instance
-# TODO
 
 # create .env for theme helper
 cd fusionauth-theme-helper
